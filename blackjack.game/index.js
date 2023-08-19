df191d16adb2e174e22df6ce49e5b0209e0e953a
Sabaudin Tozaj <116427939+st097@users.noreply.github.com>
2023-08-19 21:36:18 +0200

    Add files via upload

---
+++ blackjack.game/index.js
@@ -0,0 +1,98 @@
+let player ={
+    name : "john",
+    chips: 159
+}
+
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard 
+let hasBlackjack = false
+let isAlive = true
+let message = " "
+
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard(){
+ 
+   let randomNumber = Math.floor(Math.random()*13 + 1)
+
+   if (randomNumber > 10){
+    return 10
+   } else if(randomNumber === 1){
+    return 11
+   } else {
+    return randomNumber
+   }
+}
+
+
+function startGame(){
+    isAlive = true
+    
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
+    renderGame()
+
+}
+   
+
+function renderGame() {
+    
+    cardsEl.textContent = "Cards: " 
+    
+    for ( let i = 0 ; i < cards.length ; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+if(sum <= 20){
+    message = "Do you want to draw a new cars?"
+} 
+else if(sum === 21){
+    message = " You've got Blackjack!"
+    hasBlackjack = true
+} 
+else {
+    message = "You're out of the game!"
+    isAlive = false
+}
+
+   messageEl.textContent = message
+}
+
+function newCard(){
+
+    if(isAlive===true && hasBlackjack===false){
+    let card = getRandomCard()
+
+    sum += card
+
+    cards.push(card)
+    
+    renderGame()
+    }
+}
+
+
+
+
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+
+
+
+ 
